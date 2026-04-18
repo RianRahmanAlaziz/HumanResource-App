bc10a2f1f889e70d98119775e0b8435251931e61
Rian Rahman Al Aziz <rianrahman8243@gmail.com>
2026-04-18 10:19:57 +0700

    Fixing Bug

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
