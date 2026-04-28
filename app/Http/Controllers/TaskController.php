e61cf4752a73d47f60fe8bf378b51513d2c57d4a
RianRahmanAlaziz <rianrahman8243@gmail.com>
2026-04-28 13:38:31 +0700

    feat: Fitur Tasks - Index

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -12,7 +12,9 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+        $tasks = Task::all();
+
+        return view('dashboard.tasks.index', compact('tasks'));
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,6 +10,12 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+// Resource routes for tasks
+Route::resource('tasks', TaskController::class);
+Route::get('tasks/done/{id}', [TaskController::class, 'done'])->name('tasks.done');
+Route::get('tasks/pending/{id}', [TaskController::class, 'pending'])->name('tasks.pending');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
